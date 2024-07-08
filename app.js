f7c318e10e018bc20455d4d1871655614c30176e
Altamash <ayank829130@gmail.com>
2024-07-08 14:20:24 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const btns = document.querySelectorAll(".btn");
+let input = document.querySelector(".input");
+const deleteBtn = document.querySelector(".delete-btn");
+const equalSign = document.querySelector(".equal");
+const oneDelete = document.querySelector(".delete");
+
+btns.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    input.value += e.target.value;
+  });
+});
+
+deleteBtn.addEventListener("click", function () {
+  console.log("deleteBtn called");
+  input.value = "";
+});
+
+oneDelete.addEventListener("click", lastDelete);
+function lastDelete() {
+  let equal = input.value;
+  equal = equal.slice(0, -1);
+  input.value = equal;
+}
+
+equalSign.addEventListener("click", function () {
+  if (input.value) {
+    input.value = eval(input.value);
+  }
+});
